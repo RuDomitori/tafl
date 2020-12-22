e1549ba2068aae4dd1d2e43f3e9842d5250aaf73
RuDomitori <RuDomitori@gmail.com>
2020-12-22 18:16:55 +0700

    File check

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ namespace TAFL
     {
         static void Main(string[] args)
         {
+            if (args.Length < 1) {
+                Console.WriteLine("Не указан файл с кодом");
+                return;
+            }
+            if (!(new FileInfo(args[0]).Exists)) {
+                Console.WriteLine("Файл не найден");
+                return;
+            }
+            Console.WriteLine(args.Length);
             var lines = File.ReadAllLines(args[0]);
             var res = Lexer.Analyze(lines);
             var ops = OPSGenerator.GenerateOPS(res);
